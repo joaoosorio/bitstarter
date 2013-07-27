a2d8c6df88ab098dd1842bda1fb3e57fc6f4a60e
Ubuntu <ubuntu@ip-172-31-17-90.us-west-2.compute.internal>
2013-07-27 09:12:46 +0000

    public

--- web.js
+++ web.js
@@ -3,7 +3,7 @@ var fs = require('fs');
 
 var app = express.createServer(express.logger());
 
-app.use(express.static(__dirname + '/images'));
+app.use(express.static(__dirname + '/public'));
 
 app.get('/', function(request, response) {
   response.send(fs.readFileSync('index.html').toString());
